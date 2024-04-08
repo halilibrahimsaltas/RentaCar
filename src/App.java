2df8a837eda878b58e2af44a955d6f063d9b8f6d
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-04-08 16:46:16 +0300

    add properties

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import business.UserManager;
+import core.Db;
+import core.Helper;
+import view.AdminView;
+import view.LoginView;
+
+import java.sql.Connection;
+import  java.sql.DriverManager;
+public class App {
+    public static void main(String[] args) {
+        Helper.setTheme();
+        LoginView loginView = new LoginView();
+
+
+
+    }
+}
